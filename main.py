a438fa8cf8814f574e5200c909a630b963064cde
Your Name <you@example.com>
2023-05-11 19:26:34 +0300

    start commit

---
+++ main.py
@@ -0,0 +1,117 @@
+#def sey_hello():
+#    print('hello')
+#def add_label():
+#    label = tk.Label(win,text='Привіт Стас')
+ #   label.pack()
+#import tkinter as tk
+#win = tk.Tk()
+#win.geometry(f'400x500+100+200')
+#win.title('VJ')
+#
+#btn1 = tk.Button(win,text='Hello',
+#                 command=sey_hello
+ #                )
+
+#btn2 = tk.Button(win,text='Привіт Вася',
+ #                command=add_label
+#                 )
+
+#btn3 = tk.Button(win, text='Привіт Вася як справи',
+#                 command=lambda: tk.Label(win,text='new lambada').pack
+#                 )
+#btn1.pack()
+#btn2.pack()
+#btn3.pack()
+
+
+
+#win.mainloop()
+
+#alien_0 = {'color':'red','points':6}
+#print(alien_0['color'])
+#print(alien_0['points'])#Виводить два ключі red i 6
+
+#alien_0 = {'color':'red','points':6}
+#print(alien_0)
+#alien_0['x_position'] = 0
+#alien_0['y_position'] = 25
+#print(alien_0)
+
+#alien_0 = {}
+#alien_0['color'] = 'green'
+#alien_0['points'] = 5
+#print(alien_0)
+
+#alien_0 = {'color':'green'}
+#print(f'The alien is {alien_0["color"]}.')
+#alien_0['color'] = 'yellow'
+#print(f'The alien is now {alien_0["color"]}.')
+
+#alien_0 = {'x_position': 0, 'y_position': 25, 'speed': "medium"}
+#print(f"Original position: {alien_0['x_position']}")
+#if alien_0['speed'] == 'slow':
+#    x_increment = 1
+#elif alien_0['speed'] == 'medium':
+#    x_increment = 2
+#else:
+#    x_increment = 3
+#alien_0['x_increment'] = alien_0['x_position'] + x_increment
+#print(f"New position: {alien_0['x_position']}"
+
+
+#favorite_languages = {
+#    'jen': 'payton',
+#    'sarah': 'c',
+#    'edward': 'ruby',
+#    'phil': 'payton'
+#}
+#print(favorite_languages['edward'])
+#print(favorite_languages['jen'])
+
+#favorite_languages = {
+#    'jen': 'payton',
+#    'sarah': 'c++',
+#    'edward': 'ruby',
+#    'phil': 'payton',
+#}
+#language = favorite_languages['sarah'].title()
+#print(f"Sarah's favorite_language is {language}.")
+
+#name = {'first_name':'Іван','last_name':'Дійчук','age':'36 років','city':'Kosiv'}
+#print(name['first_name'])
+#print(name['last_name'])
+#print(name['age'])
+#print(name['city'])
+
+#favorite_numbers = {'Stas': '7','Ivan': '28','Pavlo': '9'}
+#favorite_numbers['Stas'] = '7'
+#favorite_numbers['Ivan'] = '28'
+#favorite_numbers['Pavlo'] = '9'
+#print(favorite_numbers['Stas'])
+#print(favorite_numbers['Ivan'])
+#print(favorite_numbers['Pavlo'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
